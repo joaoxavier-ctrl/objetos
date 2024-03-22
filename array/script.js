3fb1a3e6bdbb3e43ee8ea287d03cd789bbcaa015
JoaoXavierWin <joao.xavier21@gmail.com>
2024-03-22 00:40:19 -0300

    exercicio-finalizado

--- array/script.js
+++ array/script.js
@@ -4,11 +4,20 @@ const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
 // Adicione 'Arroz' ao final da array
 // Adicione 'Peixe' e 'Batata' ao início da array
 
+const primeiraComida = comidas.shift();
+const ultimaComida = comidas.pop();
+comidas.push('Arroz');
+comidas.unshift('Peixe', 'Batata');
+
 const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
 // Arrume os estudantes em ordem alfabética
 // Inverta a ordem dos estudantes
 // Verifique se Joana faz parte dos estudantes
 // Verifique se Juliana faz parte dos estudantes
+estudantes.sort();
+estudantes.reverse();
+estudantes.includes('Joana');
+estudantes.includes('Juliana');
 
 let html = `<section>
               <div>Sobre</div>
@@ -17,7 +26,19 @@ let html = `<section>
             </section>`
 // Substitua section por ul e div com li,
 // utilizando split e join
+let novohtml = html.split('section');
+novohtml = novohtml.join('ul');
+novohtml = novohtml.split('div');
+novohtml = novohtml.join('li');
+
+
 
 const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
 // Remova o último carro, mas antes de remover
 // salve a array original em outra variável
+
+const carrosOriginal = carros.slice();
+
+carros.pop();
+
+console.log(carrosOriginal);
