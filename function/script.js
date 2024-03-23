52e78721084f00a2bdcfd8020dd75eef90b2d256
JoaoXavierWin <joao.xavier21@gmail.com>
2024-03-23 00:18:52 -0300

    exercicio-finalizado

--- function/script.js
+++ function/script.js
@@ -11,9 +11,24 @@ console.log(caracteresTotal);
 // html, com os seguintes parâmetros
 // tag, classe e conteudo.
 
-function
+function novoElementos(tag, classe, conteudo){
+  const elemento = document.createElement(tag);
+  elemento.classList.add(classe);
+  elemento.innerHTML = conteudo;
+  return elemento;
+}
 
+console.log(novoElementos('ul', 'ativo', 'ablablublé'));
 
 // Crie uma nova função utilizando a anterior como base
 // essa nova função deverá sempre criar h1 com a
 // classe titulo. Porém o parâmetro conteudo continuará dinâmico
+
+function elementoEspecifico(conteudo){
+  const elementoEspecifico = document.createElement('h1');
+  elementoEspecifico.classList.add('titulo');
+  elementoEspecifico.innerHTML = conteudo;
+  return elementoEspecifico;
+}
+
+console.log(elementoEspecifico('ehuasnda'));
